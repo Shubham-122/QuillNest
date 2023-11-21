55c36d1470785346159f7f7f11bc1ddb88ef1ece
Shubham Sahai Saxena <sahaishubham1@gmail.com>
2023-11-21 11:50:29 +0530

    Adding new files

--- main.py
+++ main.py
@@ -31,6 +31,14 @@ class Contacts(db.Model):
     date = db.Column(db.String(12), nullable=True)
     
 
+class Posts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(25), nullable=False)
+    content = db.Column(db.String(500), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    img_file = db.Column(db.String(12), nullable=True)
+
 
 
 @app.route("/")
@@ -59,8 +67,13 @@ def contact():
 
     return render_template('contact.html', params=params)
 
-@app.route("/post")
-def post():
-    return render_template('post.html', params=params)
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def post_route(post_slug):
+    post= Posts.query.filter_by(slug=post_slug).first()
+
+
+
+
+    return render_template('post.html', params=params, post=post)
 
 app.run(debug = True)
